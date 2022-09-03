ea2ddd001803495277c66571815e605812275bd1
Cloudy Young <yunfan.yang@agvisorpro.com>
2022-09-02 20:19:29 -0600

    Update Lyrics.js

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -25,8 +25,8 @@ const Lyrics = ({ started }) => {
     const renderer = ({ index, active, line }) => {
         const component = components[line.content];
         return component
-            ? <div key={index} className={classNames(active ? "" : "opacity-20", "m-4 mb-20 transition-all duration-500")}>{component}</div>
-            : <div key={index} className={classNames(active ? "" : "opacity-40 scale-75", "text-white transition-all mt-4 duration-500 font-bold text-3xl origin-left ml-4 mr-10 break-normal")}>{line.content}</div>;
+            ? <div key={index} className={classNames(active ? "" : "opacity-20", "m-4 mb-20 transition-all duration-700")}>{component}</div>
+            : <div key={index} className={classNames(active ? "" : "opacity-40 scale-75", "text-white transition-all mt-4 duration-1000 font-bold text-3xl origin-left ml-4 mr-10 break-normal")}>{line.content}</div>;
     };
 
     return (
